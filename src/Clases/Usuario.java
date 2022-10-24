b560a10ca02202b88aa67b56d6aa89c24d90e297
Enrique Puente <enrique.puente@opendeusto.es>
2022-10-24 17:58:41 +0200

    Clase UsuarioTest testeada correctamente Clases de test de las demas creadas falta testearlas

--- src/Clases/Usuario.java
+++ src/Clases/Usuario.java
@@ -1,6 +1,8 @@
 package Clases;
 
-public class usuario {
+import java.util.regex.Pattern;
+
+public class Usuario {
 	private String nombre;
 	private String dni;
 	private String email;
@@ -9,41 +11,53 @@ public class usuario {
 	private boolean permisos;
 	
 	
-	public usuario(String nombre, String dni, String email, String domicilio, String contrasenia, boolean permisos) {
+	public Usuario(String nombre, String dni, String email, String domicilio, String contrasenia, boolean permisos) {
 		super();
 		this.nombre = nombre;
 		this.dni = dni;
 		this.email = email; //FALTA :metodo para autorellenar el email insertado
 		this.domicilio = domicilio;
 		this.contrasenia = contrasenia;
-		this.permisos = permisos;
+		this.permisos = false;
 	}
 	
 	
 	public String getNombre() {
 		return nombre;
 	}
+	
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
+	
 	public String getDni() {
 		return dni;
 	}
+	
 	public void setDni(String dni) {
 		this.dni = dni;
 	}
+	
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	
 	public String getDomicilio() {
 		return domicilio;
 	}
+	
 	public void setDomicilio(String domicilio) {
 		this.domicilio = domicilio;
 	}
+	
+	
 	public String getContrasenia() {
 		return contrasenia;
 	}
