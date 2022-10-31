8cd630c4429083336c37f54ca23d360c57196c31
Enrique Puente <enrique.puente@opendeusto.es>
2022-11-01 00:46:57 +0100

    Clase principal cambiado y test de la bases datos a medias hecho

--- src/Clases/BaseDatos.java
+++ src/Clases/BaseDatos.java
@@ -9,7 +9,11 @@ import java.sql.Statement;
 public class BaseDatos {
 
 
-
+	/**
+	 * Método que realiza la conexión con la base de datos
+	 * @param BBDDCliente : Nombre de la base de datos a la que nos vamos a conectar
+	 * @return Devuelve la conexión a la base de datos
+	 */
 	public static Connection initBD(String BBDDCliente) {
 		Connection con = null;
 		try {
@@ -27,6 +31,11 @@ public class BaseDatos {
 		return con;
 	}
 	
+	/**
+	 * Método que realiza la desconexión con la base de datos
+	 * @param Connection con: recibimos la conexion con la base de datos
+	 * @return No devuelve nada
+	 */
 	public static void closeBD(Connection con) {
 		if(con!=null) {
 			try {
@@ -38,8 +47,12 @@ public class BaseDatos {
 		}
 	}
 	
-	
-	public static void crearTablas(Connection con) {
+	/**
+	 * Método para crear las tablas en la BBDD 
+	 * @param Connection con: recibimos la conexion con la base de datos
+	 * @return Devuelve un boolean dependiendo si se ha realizado correctamente(true) o incorrectamente(false)
+	 */
+	public static boolean crearTablas(Connection con) {
 		String sql = "CREATE TABLE IF NOT EXISTS Usuario (nombre String, dni String, email String, domicilio String, contrasenia String, permisos int)";
 		try {
 			Statement st = con.createStatement();
@@ -49,46 +62,67 @@ public class BaseDatos {
 			sql = "CREATE TABLE IF NOT EXISTS Carrito (dniUsu String, codProd int, nomProd String, tipoProd String, marcaProd String, tamanyoProd String, precioProd double)";
 			st.executeUpdate(sql);
 			st.close();
+			return true;
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			System.err.println(String.format("ERROR AL CREAR LAS TABLAS: Usuario, Producto y Carrito", e.getMessage()));
 			e.printStackTrace();
+			return false;
 		}
 	}
-	//Metodo de insertar Usuario
+	
+	/**
+	 * Método para insertar los datos del usuario en la tabla usuario de la BBDD
+	 * @param Connection con, datos del usuario: recibimos la conexion con la base de datos y los datos del usuario
+	 * @return No devuelve nada
+	 */
 	public static void insertarUsuario(Connection con,  String nombre,  String dni,  String email,  String domicilio,  String contrasenia,  int permisos) {
 		String sql = "INSERT INTO Usuario VALUES('"+nombre+"','"+dni+"','"+email+"','"+domicilio+"','"+contrasenia+"','"+permisos+"')";
 		try {
 			Statement st = con.createStatement();
 			st.executeUpdate(sql);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			System.err.println(String.format("ERROR AL INSERTAR LOS DATOS EN LA TABLA: Usuario", e.getMessage()));
 			e.printStackTrace();
 		}
 	}
-	//Metodo de insertar Producto
+	
+	/**
+	 * Método para insertar los datos del producto en la tabla producto de la BBDD
+	 * @param Connection con, datos del producto: recibimos la conexion con la base de datos y los datos del producto
+	 * @return No devuelve nada
+	 */
 	public static void insertarProducto(Connection con,   int cod,  String nombre,  String tipo,  String marca,  String tamanyo,  double precio,  int stock) {
 		String sql = "INSERT INTO Producto VALUES('"+cod+"','"+nombre+"','"+tipo+"','"+marca+"','"+tamanyo+"',"+precio+",'"+stock+"')";
 		try {
 			Statement st = con.createStatement();
 			st.executeUpdate(sql);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			System.err.println(String.format("ERROR AL INSERTAR LOS DATOS EN LA TABLA: Producto", e.getMessage()));
 			e.printStackTrace();
 		}
 	}
-	//Metodo de insertar Carrito
+	
+	/**
+	 * Método para insertar los datos de la compra en la tabla carrito de la BBDD
+	 * @param Connection con, datos del producto: recibimos la conexion con la base de datos y los datos del producto
+	 * @return No devuelve nada
+	 */
 	public static void insertarCarrito(Connection con,   String dniUsu , int codProd, String nomProd, String tipoProd, String marcaProd, String tamanyoProd,double precioProd) {
 		String sql = "INSERT INTO Carrito VALUES('"+dniUsu+"','"+codProd+"','"+nomProd+"','"+tipoProd+"','"+marcaProd+"','"+tamanyoProd+"',"+precioProd+")";
 		try {
 			Statement st = con.createStatement();
 			st.executeUpdate(sql);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			System.err.println(String.format("ERROR AL INSERTAR LOS DATOS EN LA TABLA: Carrito", e.getMessage()));
 			e.printStackTrace();
 		}
 	}
 	
-	//Metodo de borrar Usuario
+	/**
+	 * Método para borrar los datos del Usuario si el dni recibido es el mismo que buscamos en la BBDD
+	 * @param Connection con, el dni del usuario: recibimos la conexion con la base de datos y el dni del usuario
+	 * @return No devuelve nada
+	 */
 	public static void eleminarUsuario(Connection con, String dni) {
 		String sent = "DELETE FROM Usuario WHERE dni ='"+dni+"'";
 		Statement stmt = null;
@@ -98,7 +132,7 @@ public class BaseDatos {
 			stmt.executeUpdate(sent);
 
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			System.err.println(String.format("ERROR AL BORRAR LOS DATOS DEL USUARIO", e.getMessage()));
 			e.printStackTrace();
 		}finally {
 			if (stmt != null) {
@@ -112,7 +146,11 @@ public class BaseDatos {
 		}
 	}
 	
-	//Metodo de borrar Producto
+	/**
+	 * Método para borrar los datos del producto si el codigo recibido es el mismo que buscamos en la BBDD
+	 * @param Connection con, el cod del producto: recibimos la conexion con la base de datos y el cod del producto
+	 * @return No devuelve nada
+	 */
 	public static void eleminarProducto(Connection con, int cod) {
 		String sent = "DELETE FROM Producto WHERE cod ='"+cod+"'";
 		Statement stmt = null;
@@ -122,7 +160,7 @@ public class BaseDatos {
 			stmt.executeUpdate(sent);
 
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			System.err.println(String.format("ERROR AL BORRAR LOS DATOS DEL PRODUCTO", e.getMessage()));
 			e.printStackTrace();
 		}finally {
 			if (stmt != null) {
@@ -136,7 +174,11 @@ public class BaseDatos {
 		}
 	}
 	
-	//Metodo de borrar Carrito
+	/**
+	 * Método para borrar los datos del carrito de un usuario si el dni recibido es el mismo que buscamos en la BBDD
+	 * @param Connection con, el dni del usuario : recibimos la conexion con la base de datos y el dni del usuario
+	 * @return No devuelve nada
+	 */
 	public static void eleminarCarrito(Connection con, String dni) {
 		String sent = "DELETE FROM Carrito WHERE dni ='"+dni+"'";
 		Statement stmt = null;
@@ -146,7 +188,7 @@ public class BaseDatos {
 			stmt.executeUpdate(sent);
 
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			System.err.println(String.format("ERROR AL BORRAR LOS DATOS DEL CARRITO", e.getMessage()));
 			e.printStackTrace();
 		}finally {
 			if (stmt != null) {
@@ -160,48 +202,74 @@ public class BaseDatos {
 		}
 	}
 	
-	public static Usuario obtenerUsuario(Connection con, String DNI) {
-		String sent = "SELECT * FROM Usuario WHERE dni = '"+DNI+"'";
-		Statement stmt = null;
-		Usuario u = null;
-		try {
-			stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery(sent);
-			if (rs.next()) {
-				String nombre = rs.getString("nombre");
-				String dni = rs.getString("dni");
-				String email = rs.getString("email");
-				String domicilio = rs.getString("domicilio");
-				String contrasenia = rs.getString("contrasenia");
-				int permisos = rs.getInt("permisos");
-				u = new Usuario(nombre, dni, email, domicilio, contrasenia, permisos);
+	/**
+	 * Método para buscar los datos de un usuario si el dni recibido es el mismo que buscamos en la BBDD
+	 * @param Connection con, el dni de un usuario : recibimos la conexion con la base de datos y el dni del usuario
+	 * @return Devuelve el usuario buscado
+	 */
+	public static boolean obtenerUsuario(Connection con, String DNI) {
+		boolean encontrada = false;
+		String sent = "SELECT nombre FROM Usuario WHERE dni = '"+DNI+"'";
+		try (Statement st = con.createStatement();){
+			ResultSet rs = st.executeQuery(sent);
+			if(rs.next()) {
+				encontrada = true;
 			}
 			rs.close();
-			
-			
-			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			System.err.println(String.format("ERROR AL ENCONTRAR LOS DATOS DEL USUARIO", e.getMessage()));
 			e.printStackTrace();
-		}finally {
-			if (stmt != null) {
-				try {
-					stmt.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}return u;	
+		}
+		return encontrada;
 	}
 	
+//	public static Usuario obtenerUsuario(Connection con, String DNI) {
+//		String sent = "SELECT * FROM Usuario WHERE dni = '"+DNI+"'";
+//		Statement stmt = null;
+//		Usuario u = null;
+//		try {
+//			stmt = con.createStatement();
+//			ResultSet rs = stmt.executeQuery(sent);
+//			if (rs.next()) {
+//				String nombre = rs.getString("nombre");
+//				String dni = rs.getString("dni");
+//				String email = rs.getString("email");
+//				String domicilio = rs.getString("domicilio");
+//				String contrasenia = rs.getString("contrasenia");
+//				int permisos = rs.getInt("permisos");
+//				u = new Usuario(nombre, dni, email, domicilio, contrasenia, permisos);
+//			}
+//			rs.close();
+//			
+//			
+//			
+//		} catch (SQLException e) {
+//			System.err.println(String.format("ERROR AL ENCONTRAR LOS DATOS DEL USUARIO", e.getMessage()));
+//			e.printStackTrace();
+//		}finally {
+//			if (stmt != null) {
+//				try {
+//					stmt.close();
+//				} catch (SQLException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//			}
+//		}return u;	
+//	}
+	
+	/**
+	 * Método para modificar los datos de un usuario si el dni recibido y la contrasenia son los mismos que buscamos en la BBDD
+	 * @param Connection con, el dni de un usuario y su contrasenia : recibimos la conexion con la base de datos, el dni del usuario y su contrasenia
+	 * @return No devuelve nada
+	 */
 	public static void modificarConUsuario(Connection con, String DNI, String contr){
 		String sent = "UPDATE Usuario SET contrasenia='"+contr+"' where dni = '"+DNI+"'";
 		Statement stmt = null;
 		try {
 			stmt = con.createStatement();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			System.err.println(String.format("ERROR AL MODIFICAR LOS DATOS DEL USUARIO", e.getMessage()));
 			e.printStackTrace();
 		}
 		try {
--- testClases/Clases/BaseDatosTest.java
+++ testClases/Clases/BaseDatosTest.java
@@ -2,22 +2,52 @@ package Clases;
 
 import static org.junit.Assert.*;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class BaseDatosTest {
-
+	private Connection con;
+	
 	@Before
 	public void setUp() throws Exception {
+		con = BaseDatos.initBD("data/DeustoIkea.db");
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		BaseDatos.closeBD(con);
 	}
 
 	@Test
-	public void test() {
+	public void testInitBD() {
+		assertNotNull(con);
 	}
+	
+	@Test
+	public void testCloseBD() {
+	
+	}
+	
+	@Test
+	public void testCrearTablas() {
+		boolean correcto = BaseDatos.crearTablas(con);
+		assertTrue(correcto);
+	}
+	
+	@Test
+	public void testObtenerUsuario() {
+		boolean encontrada = BaseDatos.obtenerUsuario(con, "111A");
+		assertTrue(encontrada);
+	}
+	
+	@Test
+	public void testInsertarUsuario() {
+		
+		
 
+	}
 }
