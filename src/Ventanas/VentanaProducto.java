b37d66000723bfd4ab222f60e290fb86f63a600d
ismaelalcaide <91288325+ismaelalcaide@users.noreply.github.com>
2022-11-21 11:46:18 +0100

    dos ventanas

---
+++ src/Ventanas/VentanaProducto.java
@@ -0,0 +1,43 @@
+package Ventanas;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+
+public class VentanaProducto extends JFrame{
+
+	private JFrame vent;
+	
+	public VentanaProducto() {
+		
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.NORTH);
+		
+		JLabel lblNewLabel = new JLabel("LISTA PRODUCTO CON INFORMACION");
+		panel.add(lblNewLabel);
+		
+		JPanel panel_1 = new JPanel();
+		getContentPane().add(panel_1, BorderLayout.SOUTH);
+		panel_1.setLayout(new GridLayout(1, 4, 0, 0));
+		
+		JButton btnEliminar = new JButton("Eliminar");
+		panel_1.add(btnEliminar);
+		
+		JButton btnAnadir = new JButton("Añadir");
+		panel_1.add(btnAnadir);
+		
+		JButton btnActualizar = new JButton("Actualizar");
+		panel_1.add(btnActualizar);
+		
+		JButton btnSalir = new JButton("Salir");
+		panel_1.add(btnSalir);
+
+		
+		JPanel panel_2 = new JPanel();
+		getContentPane().add(panel_2, BorderLayout.CENTER);
+
+	}
+}
---
+++ src/Ventanas/VentanaVentas.java
@@ -0,0 +1,32 @@
+package Ventanas;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+public class VentanaVentas extends JFrame{
+	
+	private JFrame vent;
+	
+	public VentanaVentas() {
+		
+		JPanel panel = new JPanel();	
+		getContentPane().add(panel, BorderLayout.NORTH);
+		
+		JLabel lblVentas = new JLabel("VENTAS");
+		panel.add(lblVentas);
+		
+		JPanel panel_1 = new JPanel();
+		getContentPane().add(panel_1, BorderLayout.SOUTH);
+		
+		JButton btnAtras = new JButton("Atras");
+		panel_1.add(btnAtras);
+		
+		JPanel panel_2 = new JPanel();
+		getContentPane().add(panel_2, BorderLayout.CENTER);
+		
+
+	}
+}
