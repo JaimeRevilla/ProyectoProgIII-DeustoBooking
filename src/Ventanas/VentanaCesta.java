4e3655c761e4341a732078f8d98bdf2155b0e9fa
ismaelalcaide <91288325+ismaelalcaide@users.noreply.github.com>
2022-12-11 14:18:03 +0100

    fichero

--- src/Ventanas/VentanaCesta.java
+++ src/Ventanas/VentanaCesta.java
@@ -12,12 +12,15 @@ import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
 import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
+
 import java.awt.CardLayout;
 import javax.swing.JPasswordField;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import java.awt.Component;
@@ -31,10 +34,18 @@ import Clases.Carrito;
 import Clases.Producto;
 
 import java.awt.event.ActionListener;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.regex.Pattern;
 import java.awt.event.ActionEvent;
@@ -47,6 +58,11 @@ public class VentanaCesta extends JFrame {
 	private DefaultTableModel model;
 	private JTable tablaCesta;
 	private JScrollPane scrollTabla;
+
+	private JList<Carrito> listaFactura;
+	private DefaultListModel<Carrito> modelLista;
+	private JScrollPane panelLista;
+	
 	
 	public VentanaCesta() {
 		
@@ -83,7 +99,7 @@ public class VentanaCesta extends JFrame {
 		
 		
 		
-		JButton btnFactura = new JButton("FACTURA");
+		JButton btnFactura = new JButton("VER FACTURA");
 		
 		panelCentroEste.add(btnFactura);
 		
@@ -131,9 +147,8 @@ public class VentanaCesta extends JFrame {
 		btnBorrarProducto.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				int fila = tablaCesta.getSelectedRow();
-				String valor = tablaCesta.getValueAt(fila, 3).toString();
-				try {
 					if(tablaCesta.getSelectedRow()>=0) {
+						String valor = tablaCesta.getValueAt(fila, 0).toString();
 						model = (DefaultTableModel)tablaCesta.getModel();
 						model.removeRow(tablaCesta.getSelectedRow());	
 						BaseDatos.eliminarFilaPorCodigoProd(con, valor);
@@ -142,9 +157,9 @@ public class VentanaCesta extends JFrame {
 					}else {
 						JOptionPane.showMessageDialog(null, "Para eliminar un producto seleccione el producto");
 					}
-				}catch(Exception ex) {
-					JOptionPane.showMessageDialog(null, "Para eliminar un producto seleccione el producto");
-				}
+//				}catch(Exception ex) {
+//					JOptionPane.showMessageDialog(null, "Para eliminar un producto seleccione el producto");
+//				}
 				
 //				if (tablaCesta.getSelectedRow()>=0) {
 //					model = (DefaultTableModel)tablaCesta.getModel();
@@ -157,13 +172,24 @@ public class VentanaCesta extends JFrame {
 			}
 		});
 		
+		btnNewButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Factura();
+				
+			}
+		});
+		
 		panelCentroEste.add(btnBorrarProducto);
 		
 		panelCentro.add(scrollTabla, BorderLayout.CENTER);
 		
 		btnFactura.addActionListener(new ActionListener() {
+			@Override
 			public void actionPerformed(ActionEvent e) {
 				VentanaFactura v1 = new VentanaFactura();
+				cargarFichero();
 			}
 		});
 		
@@ -177,6 +203,76 @@ public class VentanaCesta extends JFrame {
 		
 		setVisible(true);
 
+	}
+	
+	private void Factura() {
+		try (PrintWriter pw = new PrintWriter(new FileOutputStream("Factura"+VentanaInicioSesion.dni+".txt"));){
+			Date fechaSistema = new Date(System.currentTimeMillis());
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			pw.println(sdf.format(fechaSistema));
+			for(int i=0; i<model.getRowCount();i++) {
+				String dni = (String) model.getValueAt(i, 0);
+				int cod = (int) model.getValueAt(i, 1);
+				String nom = (String) model.getValueAt(i, 2);
+				String tipo = (String) model.getValueAt(i, 3);
+				String marca = (String) model.getValueAt(i, 4);
+				String tam = (String) model.getValueAt(i, 5);
+				double precio = (double) model.getValueAt(i, 6);
+				pw.println(dni+";"+cod+";"+nom+";"+tipo+";"+marca+";"+tam+";"+precio);
+				System.out.println(String.format("El fichero se ha creado correctamente para el:%s",VentanaInicioSesion.dni));
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println(String.format("Error cesta:%s",e.getMessage()));
+			e.printStackTrace();
+		}
+	}
+	
+//	private void Factura() {
+//		ObjectOutputStream oos = null;
+//		
+//		try {
+//			FileOutputStream fos = new FileOutputStream(VentanaInicioSesion.dni+".txt");
+//			BufferedOutputStream bos = new BufferedOutputStream(fos);
+//			oos = new ObjectOutputStream(bos);
+//			
+//			for (Producto p : VentanaInicioSesion.carrito) {
+//				oos.writeObject(p);
+//			}
+//			
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} finally {
+//			if(oos!=null) {
+//				try {
+//					oos.close();
+//				} catch (IOException e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		}
+//	
+//	}
+	
+	private void cargarFichero() {
+		try (BufferedReader br = new BufferedReader(new FileReader("Factura"+VentanaInicioSesion.dni+".txt"));){
+			String linea = br.readLine();
+			while(linea!=null) {
+				String [] dato = linea.split(";");
+				Carrito ca = new Carrito(dato[0],Integer.parseInt((String)dato[1]),dato[2],dato[3],dato[4],dato[5],Double.parseDouble((String)dato[6]));
+				System.out.println(ca);
+				modelLista.addElement(ca);
+				linea = br.readLine();
+				System.out.println(linea);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println(String.format("Error Factura:%s",e.getMessage()));
+			e.printStackTrace();
+		} catch (IOException e1) {
+			System.out.println(String.format("Error Factura1:%s",e1.getMessage()));
+			e1.printStackTrace();
+		}
 	}
 	
 	
--- src/Ventanas/VentanaFactura.java
+++ src/Ventanas/VentanaFactura.java
@@ -1,19 +1,33 @@
 package Ventanas;
 
 import java.awt.EventQueue;
+import java.awt.ScrollPane;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
 import java.awt.BorderLayout;
+
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
-import javax.swing.JTable;
+
+import Clases.Carrito;
+
 import javax.swing.JLabel;
+import javax.swing.JList;
+
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 
 public class VentanaFactura extends JFrame{
-	private JTable tablaFactura;
-	
+	private JList<Carrito> listaFactura;
+	private DefaultListModel<Carrito> model;
+	private JScrollPane panelLista;
 	
 	
 	public VentanaFactura() {
@@ -29,9 +43,12 @@ public class VentanaFactura extends JFrame{
 		
 		JPanel panelCentro = new JPanel();
 		getContentPane().add(panelCentro, BorderLayout.CENTER);
-		
-		tablaFactura = new JTable();
-		panelCentro.add(tablaFactura);
+
+		model = new DefaultListModel<>();
+		listaFactura = new JList<>(model);
+		panelLista = new JScrollPane(listaFactura);
+		panelLista.add(listaFactura);
+		cargarFichero();
 		
 		JPanel panelNorte = new JPanel();
 		getContentPane().add(panelNorte, BorderLayout.NORTH);
@@ -44,9 +61,30 @@ public class VentanaFactura extends JFrame{
 				dispose();
 			}
 		});
+		
+		
 		setVisible(true);
 	}
 
+	private void cargarFichero() {
+		try (BufferedReader br = new BufferedReader(new FileReader("Factura"+VentanaInicioSesion.dni+".txt"));){
+			String linea = br.readLine();
+			while(linea!=null) {
+				String [] dato = linea.split(";");
+				Carrito ca = new Carrito(dato[0],Integer.parseInt((String)dato[1]),dato[2],dato[3],dato[4],dato[5],Double.parseDouble((String)dato[6]));
+				System.out.println(ca);
+				model.addElement(ca);
+				linea = br.readLine();
+				System.out.println(linea);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println(String.format("Error Factura:%s",e.getMessage()));
+			e.printStackTrace();
+		} catch (IOException e1) {
+			System.out.println(String.format("Error Factura1:%s",e1.getMessage()));
+			e1.printStackTrace();
+		}
+	}
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
