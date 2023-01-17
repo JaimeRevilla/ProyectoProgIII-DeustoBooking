08056b199c28b559c4aeb82fa4e1565703e213f6
JaimeRevilla <jaimerevilla@opendeusto.es>
2023-01-17 20:24:25 +0100

    Update BaseDatosTest.java

--- testClases/Clases/BaseDatosTest.java
+++ testClases/Clases/BaseDatosTest.java
@@ -3,6 +3,7 @@ package Clases;
 import static org.junit.Assert.*;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import org.junit.After;
@@ -11,43 +12,94 @@ import org.junit.Test;
 
 public class BaseDatosTest {
 	private Connection con;
-	
+
 	@Before
 	public void setUp() throws Exception {
-		con = BaseDatos.initBD("data/DeustoIkea.db");
+		
+		con = BaseDatos.initBD("DeustoIkea.db");
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		BaseDatos.closeBD(con);
+		con.close();
 	}
 
 	@Test
 	public void testInitBD() {
 		assertNotNull(con);
 	}
-	
+
 	@Test
-	public void testCloseBD() {
-	
+	public void testCrearTablasUsuario() {
+		BaseDatos.crearTablasUsuario(con);
+		assertNotNull(con);
 	}
-	
+
 	@Test
-	public void testCrearTablas() {
-		boolean correcto = BaseDatos.crearTablasUsuario(con);
-		assertTrue(correcto);
+	public void testCrearTablasProducto() {
+		BaseDatos.crearTablasProducto(con);
+		assertNotNull(con);
 	}
-	
-//	@Test
-//	public void testObtenerUsuario() {
-//		Usuario encontrada = BaseDatos.obtenerUsuario(con, "111A");
-//		assertTrue(encontrada);
-//	}
-	
+
+	@Test
+	public void testCrearTablasCarrito() {
+		BaseDatos.crearTablasCarrito(con);
+		assertNotNull(con);
+	}
+
 	@Test
 	public void testInsertarUsuario() {
-		
-		
-
+		assertThrows(AssertionError.class, ()->{assertThrows(SQLException.class, ()->{BaseDatos.insertarUsuario(con, "daniel","111A","dani@mail.com","caceres","111B",0);});});
 	}
+
+	@Test
+	public void testInsertarProducto() {
+		assertThrows(AssertionError.class, ()->{assertThrows(SQLException.class, ()->{BaseDatos.insertarProducto(con, 0, "dadsas", "asdsad", "adasd", "adsd", 3.3, 3, null);});});
+	}
+
+	@Test
+	public void testInsertarCarrito() {
+		assertThrows(AssertionError.class, ()->{assertThrows(SQLException.class, ()->{BaseDatos.insertarCarrito(con, "111A", 3, "asdsd", "asdasd", "asds", "adsasd", 3, 3.3);});});
+	}
+
+
+
+	@Test
+	public void testBuscarUsuario() {
+		boolean encontrada = BaseDatos.buscarUsuario(con, "111A");
+		assertTrue(encontrada);	
+	}
+
+	@Test
+	public void testObtenerContrasena() {
+		String contr = BaseDatos.obtenerContrasena(con, "111A");
+		assertEquals("111B", contr);
+	}
+	
+	@Test
+	public void testObtenerProductoTipo() {
+		ArrayList<Producto> prod = BaseDatos.obtenerProductoTipo(con, "ELECTRICA");
+		assertEquals(0, prod.size());
+	}
+
+
+	@Test
+	public void testRestarUnidadesAProducto() {
+		Producto p = new Producto(0, "abcd", null, null, null, 0, 10, null);
+		BaseDatos.restarUnidadesAProducto(con, "abcd", 2);
+		assertEquals(8, p.getStock());
+	}
+
+	@Test
+	public void testObtenerStockProducto() {
+		int cant = BaseDatos.obtenerStockProducto(con, "Sofa");
+		assertEquals(200, cant);
+	}
+
+	@Test
+	public void testObtenerPrecioProducto() {
+		fail("Not yet implemented");
+	}
+
+
 }
